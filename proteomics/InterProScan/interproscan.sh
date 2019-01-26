9e664186c330e085e675094458a7b2e9af667a93
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-01-25 22:52:08 -0500

    Script to scan for protein domain motifs.

--- proteomics/InterProScan/interproscan.sh
+++ proteomics/InterProScan/interproscan.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -l
+#PBS -N interpro
+#PBS -q standby
+#PBS -l nodes=1:ppn=20,walltime=4:00:00
 
-# More to follow.
+# Load modules
+module load bioinfo java/8
 
-./interproscan.sh -i test_proteins.fasta -f tsv
+WRK_DIR="$RCAC_SCRATCH/genomes/Triticum_aestivum/ensembl_IWSGC/pep"
+IPS_DIR="$RCAC_SCRATCH/tools/interproscan-5.33-72.0"
+
+cd "${WRK_DIR}"
+
+for file in ${WRK_DIR}/Triticum_aestivum.IWGSC.pep.all.{00..39}.fa
+do
+    $IPS_DIR/interproscan.sh\
+        --cpu $PBS_NUM_PPN \
+        -i $file \
+        -f tsv \
+        -o "${file}.interproscan-5.33-72.0.tsv"
+done
