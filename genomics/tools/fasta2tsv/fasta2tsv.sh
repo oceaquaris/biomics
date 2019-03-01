94e46e60a7c3606d695a86cf57bc59bf568873c3
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-03-01 15:26:28 -0500

    Convert fasta with custom header to tsv.

--- genomics/tools/fasta2tsv/fasta2tsv.sh
+++ genomics/tools/fasta2tsv/fasta2tsv.sh
@@ -14,5 +14,7 @@ then
                     printf("%s\t", substr($0,2,length($0)-1))
             else
                 printf("%s", $0)
-        }END{printf("\n")}' $1 | sed 's/ /\t/g'
+        }END{printf("\n")}' $1 | tee /tmp/fasta.tsv | cut -f 2 > /tmp/fasta.seqs
+    cut -d $'\t' -f 1 /tmp/fasta.tsv | sed 's/ pep/\tpep/g; s/ chromosome:/\tchromosome:/g; s/ gene:/\tgene:/g; s/ transcript:/\ttranscript:/g; s/ gene_biotype:/\tgene_biotype:/g; s/ transcript_biotype:/\ttranscript_biotype:/g; s/ description:/\tdescription:/g' > /tmp/info.tsv
+    paste <(cut -f 1,2,3,4,5,6,7 /tmp/info.tsv) <(cut -f 8 /tmp/info.tsv) /tmp/fasta.seqs
 fi
