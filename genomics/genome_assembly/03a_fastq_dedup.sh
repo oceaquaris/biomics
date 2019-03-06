c237cf66f28c96466b4a16bd07b41df15861423d
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-03-06 11:20:17 -0500

    changes in variables.

--- genomics/genome_assembly/03a_fastq_dedup.sh
+++ genomics/genome_assembly/03a_fastq_dedup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N tsvsplit
+#PBS -N fastqdedup
 #PBS -q standby
 #PBS -l nodes=1:ppn=20,walltime=4:00:00,naccesspolicy=singlejob
 #PBS -l epilogue=/scratch/snyder/r/rshrote/epilogue.sh
@@ -7,9 +7,9 @@
 cd $PBS_O_WORKDIR
 
 FASTUNIQ="$RCAC_SCRATCH/tools/FastUniq/source/fastuniq"
-INPUT_LIST="$RCAC_SCRATCH/list.txt"
-R1_OUTPUT="$RCAC_SCRATCH/R1.dedup.fastq"
-R2_OUTPUT="$RCAC_SCRATCH/R2.dedup.fastq"
+INPUT_LIST="$RCAC_SCRATCH/assembly/03_dedup/set1.txt"
+R1_OUTPUT="$RCAC_SCRATCH/assembly/03_dedup/dedup/set1.R1.dedup.fastq"
+R2_OUTPUT="$RCAC_SCRATCH/assembly/03_dedup/dedup/set1.R2.dedup.fastq"
 
 ${FASTUNIQ} \
     -i "${INPUT_LIST}" \
