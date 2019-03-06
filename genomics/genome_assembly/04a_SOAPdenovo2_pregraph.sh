9c19ee459295d448567443666c4f221e9152f5a7
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-03-06 15:30:05 -0500

    Improve pregraph commands.

--- genomics/genome_assembly/04a_SOAPdenovo2_pregraph.sh
+++ genomics/genome_assembly/04a_SOAPdenovo2_pregraph.sh
@@ -7,7 +7,9 @@
 cd $PBS_O_WORKDIR
 
 BINARY="$RCAC_SCRATCH/tools/SOAPdenovo2/SOAPdenovo-127mer"
-CONFIG_FILE="04_assembly.config"
+CONFIG_FILE="$RCAC_SCRATCH/assembly/04_SOAPdenovo2/04_assembly.config"
+# in GB!
+INIT_MEMORY_ASSUMPTION=150
 KMER_SIZE=89
 #KMER_SIZE=75
 #KMER_SIZE=105
@@ -16,9 +18,9 @@ KMER_SIZE=89
 ${BINARY} \
     pregraph \
     -s "${CONFIG_FILE}" \
+    -o "pregraph_${KMER_SIZE}mer" \
     -K ${KMER_SIZE} \
     -p $PBS_NUM_PPN \
     -R \
-    -o "pregraph_${KMER_SIZE}mer" \
     1>"pregraph_${KMER_SIZE}mer.log" \
     2>"pregraph_${KMER_SIZE}mer.err"
