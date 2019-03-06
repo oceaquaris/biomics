323242083327a5baaf799c63941f63b6e193aad0
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-03-05 22:14:52 -0500

    map script.

---
+++ genomics/genome_assembly/04d_SOAPdenovo2_map.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#PBS -N map
+#PBS -q mohamm20
+#PBS -l nodes=1:ppn=20,walltime=336:00:00,naccesspolicy=singlejob
+#PBS -l epilogue=/scratch/snyder/r/rshrote/epilogue.sh
+
+cd $PBS_O_WORKDIR
+
+BINARY="$RCAC_SCRATCH/tools/SOAPdenovo2/SOAPdenovo-127mer"
+CONFIG_FILE="04_assembly.config"
+KMER_SIZE=89
+#KMER_SIZE=75
+#KMER_SIZE=105
+GRAPH_PREFIX="pregraph_${KMER_SIZE}mer"
+
+${BINARY} \
+    map \
+    -s "${CONFIG_FILE}" \
+    -g "${GRAPH_PREFIX}" \
+    1>"map_${KMER_SIZE}.log" \
+    2>"map_${KMER_SIZE}.err"
