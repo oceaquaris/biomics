d5bf31ae83d70ae7f6ff1fb79f80690439ade0a6
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-03-07 22:52:35 -0500

    Improve simple wget script.

--- genomics/genome_assembly/01_wget_genome.sh
+++ genomics/genome_assembly/01_wget_genome.sh
@@ -1,11 +1,47 @@
 #!/bin/bash -l
 #PBS -N wget
 #PBS -q standby
-#PBS -l nodes=1:ppn=1,walltime=4:00:00
+#PBS -l nodes=1:ppn=1,walltime=4:00:00,naccesspolicy=singleuser
 
-cd $RCAC_SCRATCH/genomes/
+################################################################################
+### Script configuration variables and constants.
+############################################################
+### Working directory variables
+# Path for working directory where genomes are to be outputted.
+wrkdir="${RCAC_SCRATCH}/genomes/"
+############################################################
 
+############################################################
+### Paths needed for script i/o
+# Path to most recent Triticum aestivum genome.
 DIRECTORY="ftp://ftp.ensemblgenomes.org/pub/plants/release-42/fasta/triticum_aestivum/"
+############################################################
+################################################################################
 
+
+
+################################################################################
+################################### Commands ###################################
+################################################################################
+
+############################################################
+# Change working directory to the desired location
+############################################################
+cd "${wrkdir}"
+
+
+############################################################
 # Download entire wheat genome directory from Ensembl Plants.
+### wget
+#    Summary:
+#     Retrieve web pages or files via HTTP, HTTPS or FTP.
+#    Arguments (selective list):
+#     -r, --recursive
+#           Turn on recursive retrieving. The default maximum depth is 5.
+#     -np, --no-parent
+#           Don't ascend to the parent directory when downloading.
+############################################################
 wget -r -np "${DIRECTORY}"
+
+
+# End of Operations; EOF
